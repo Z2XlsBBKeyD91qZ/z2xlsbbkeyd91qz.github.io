bf24ae594c45263cc4637c0eca57a631f1d50998
Z2XlsBBKeyD91qZ <85584592+Z2XlsBBKeyD91qZ@users.noreply.github.com>
2021-06-08 15:44:37 -0400

    Add files via upload

--- Locals/Main-Locals.lua
+++ Locals/Main-Locals.lua
@@ -17,3 +17,33 @@ local SettingsButton = ButtonLayout.sButton
 local Page1 = Pages.Page1
 local Page2 = Pages.Page2
 local Page3 = Pages.Page3
+local Plr = game.Players.LocalPlayer
+local Players = game.Players
+local Mouse = Plr:GetMouse()
+local Camera = game.Workspace.CurrentCamera
+local TeamCheck = true
+
+local ESP = {
+    Enabled = false,
+    Boxes = true,
+    BoxShift = CFrame.new(0,-1.5,0),
+	BoxSize = Vector3.new(4,6,0),
+    Color = Color3.fromRGB(104, 67, 150),
+    FaceCamera = false,
+    Names = false,
+    TeamColor = false,
+    Thickness = 2,
+    AttachShift = 1,
+    TeamMates = false,
+    Players = true,
+    
+    Objects = setmetatable({}, {__mode="kv"}),
+    Overrides = {}
+}
+local plrs = Players
+local plr = Plr
+local mouse = Mouse
+local V3new = Vector3.new
+local WorldToViewportPoint = Camera.WorldToViewportPoint
+local boxBase = {}
+local color
